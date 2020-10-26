e11d19cc2a6282e6a7f30ef54991c73cf8b9397e
z425589643 <zt425589643@gmail.com>
2020-10-26 11:29:26 +0800

    更新

---
+++ src/GlobalIntializer.swift
@@ -0,0 +1,14 @@
+import Foundation
+import Resolver
+
+struct GlobalIntializer {
+    private static let _initialized: Bool = {
+        Resolver.queue = QueueFactory.getQueue()
+        return true
+    }()
+
+    
+    static func initalize() {
+        _ = _initialized
+    }
+}
